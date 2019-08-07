005934396e8cb1488e7bcb338709ef75cba64ade
elenalev <53762490+elenalev@users.noreply.github.com>
2019-08-07 05:46:10 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,36 +1,36 @@
 "use strict";
-
 var githubApp = angular.module('app', []);
-githubApp.controller('gitApi', ['$scope', '$http', '$q', function($scope, $http, $q) {
+githubApp.controller('gitApi', ['$scope', '$http', '$q', function ($scope, $http, $q) {
+  // scope var searchRepository default value for the page load
   $scope.searchRepository = 'github';
-  $http.get("https://api.github.com/users/"+ $scope.searchRepository)
-    .success(function(data) {
+  $http.get("https://api.github.com/users/" + $scope.searchRepository)
+    .success(function (data) {
       $scope.userData = data;
       loadOrgsRepos();
     });
-  
-  $scope.GetRepositories = function() {
-      loadOrgsRepos();
+  // GetRepositories function definition called by ng-click
+  $scope.GetRepositories = function () {
+    loadOrgsRepos();
   };
-  var loadOrgsRepos = function() {
-    $http.get("https://api.github.com/search/repositories?q="+ $scope.searchRepository +"+in:name")
-      .success(function(data) {
+  var loadOrgsRepos = function () {
+    $http.get("https://api.github.com/search/repositories?q=" + $scope.searchRepository + "+in:name")
+      .success(function (data) {
 
         $scope.orgsData = data.items;
-
+        // arrange received data in contribs array
         var contribs = [];
         for (var i in data.items) {
           contribs.push(data.items[i].contributors_url);
         }
-
-        $q.all(contribs.map(function(item) {
-            return $http({
-              method: 'GET',
-              url: item
-            });
-          }))
-          .then(function(results) {
-            results.forEach(function(val, i) {
+        // arrange orgsData scope variable to use in repeater
+        $q.all(contribs.map(function (item) {
+          return $http({
+            method: 'GET',
+            url: item
+          });
+        }))
+          .then(function (results) {
+            results.forEach(function (val, i) {
               $scope.orgsData[i].contributors = val.data;
             });
           });
